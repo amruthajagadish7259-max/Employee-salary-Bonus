e8151dd377f755f071426041d96dc9d6a2e0ac3e
amruthajagadish7259-max <amruthajagadish7259@gmail.com>
2025-11-11 10:07:43 +0530

    Update empsalbon.py

--- empsalbon.py
+++ empsalbon.py
@@ -5,7 +5,5 @@ if len(sys.argv)==2:
 else:
     salary=50000
 
-    bonus=salary+salary*(10/100)
-
-
-    print("Total salary after adding bonus:",bonus)
+bonus=salary+salary*(10/100)
+print("Total salary after adding bonus:",bonus)
